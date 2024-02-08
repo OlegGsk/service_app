d75ee9672c3f5543d1ba5e8a830e0828434484d0
Олег Говоровский <oleq.gov@ya.ru>
2024-02-08 20:01:44 +0300

    Day 4-2

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -13,12 +13,12 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
+    # price = serializers.FloatField(read_only=True)
     price = serializers.SerializerMethodField()
 
 
     def get_price(self, instance):
-        return (instance.service.full_price -
-                (instance.service.full_price * instance.plan.discount_percent / 100))
+        return instance.price
 
     class Meta:
         model = Subscription
