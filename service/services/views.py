85f32aada03694a8b79caba5f68f3e708aa14a65
Олег Говоровский <oleq.gov@ya.ru>
2024-02-08 20:49:20 +0300

    Day 5-1

--- service/services/views.py
+++ service/services/views.py
@@ -19,6 +19,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
+
         response_data = {'result': response.data}
         response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
         response.data = response_data
